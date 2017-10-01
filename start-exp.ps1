4070e1eeaefb3f05a2146a6dbadce6410244c249
Noah Mercer <noah_ten@yahoo.com>
2017-10-01 16:25:16 -0700

    adds files for starting emwave and our app at machine startup

---
+++ start-exp.ps1
@@ -0,0 +1,3 @@
+﻿$emwv = start-process -WindowStyle Hidden -PassThru "C:\Program Files (x86)\HeartMath\emWave\emwavepc.exe"
+
+start-process -WindowStyle Maximized -ArgumentList "$($emwv.Id)" "C:\Users\HRVT_7\Documents\Executable_8_29\emwave_pulse_PC_App.exe"
